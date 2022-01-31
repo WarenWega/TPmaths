ab4e5d75acfa4af96f6a9726fe76d15f75eebc6a
warencondor1 <73301658+WarenWega@users.noreply.github.com>
2022-01-31 21:46:25 +0100

    Add files via upload

---
+++ Test1_Projet_matha.m
@@ -0,0 +1,22 @@
+ a =record(5) ;
+ b =fft(a) ;
+ t=1:1:40000 ;
+ figure(1)
+ plot(t,a)
+ 
+ figure(2)
+ plot(t,b)
+ 
+ for i=201:19800 
+   b(i)=b(i-200);
+   b(40000-i+1)=b(40000-i+200);
+ endfor
+ 
+ for y=1:200
+   b(y) = i*0;
+   b(40000-y+1) = i*0;
+ endfor
+
+ c=ifft(b) ;
+figure(3)
+plot (t,c) 
